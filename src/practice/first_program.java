6c34e5a1d5c86c9395c3457e4d786843444722cc
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2023-03-06 20:56:24 +0530

    first commit

---
+++ src/practice/first_program.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class first_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first program");
+		
+
+	}
+
+}
